a3481d5fe9ccd6c2cc329fb420bcfb8b802e5a14
Andre Busche <busche@ismll.de>
2020-10-15 13:03:34 +0200

    Use proper interface file names in target root fs
    Use interface file names as used in the official documentation of pigpiod (http://abyz.me.uk/rpi/pigpio/pdif2.html)

--- gpioexpand/package/pigpio/pigpio.mk
+++ gpioexpand/package/pigpio/pigpio.mk
@@ -20,8 +20,8 @@ endef
 #
 define PIGPIO_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/pigpio.h $(STAGING_DIR)/usr/include/pigpio.h
-	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if.h $(STAGING_DIR)/usr/include/pigpio_if.h
-	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if2.h $(STAGING_DIR)/usr/include/pigpio_if2.h
+	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if.h $(STAGING_DIR)/usr/include/pigpiod_if.h
+	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if2.h $(STAGING_DIR)/usr/include/pigpiod_if2.h
 	$(INSTALL) -D -m 0755 $(@D)/libpigpio.so* $(STAGING_DIR)/usr/lib
 	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if.so* $(STAGING_DIR)/usr/lib
 	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if2.so* $(STAGING_DIR)/usr/lib
